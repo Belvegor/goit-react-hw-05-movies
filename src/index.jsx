de5c71e541f9b66d4659c1997f61c1eb2c8a5743
Igor <igor_socha@yahoo.com>
2024-02-20 09:52:56 +0100

    v.1.0.2

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import { Suspense, lazy } from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Spinner } from "./components/Spinner/Spinner";
 import { Shared } from "./shared/Shared";
 import "./index.css";
@@ -11,19 +11,19 @@ const MovieDetails = lazy(() => import("./pages/MovieDetails/MovieDetails"));
 const Cast = lazy(() => import("./components/Cast/Cast"));
 const Reviews = lazy(() => import("./components/Reviews/Reviews"));
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/goit-react-hw-05-movies/">
+createRoot(document.getElementById("root")).render(
+  <Router basename="/goit-react-hw-05-movies">
     <Shared>
       <Suspense fallback={<Spinner />}>
         <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/movies" element={<Movies />}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
           <Route path="/movies/:movieId" element={<MovieDetails />}>
-            <Route path="cast" element={<Cast />}/>
-            <Route path="reviews" element={<Reviews />}/>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
         </Routes>
       </Suspense>
     </Shared>
-  </BrowserRouter>
+  </Router>
 );
